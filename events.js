300fe6a803d725f40f3e17fa77494eb265f14408
Josh Kuhar <jdkuhar@gmail.com>
2016-09-27 16:55:14 -0400

    update

--- events.js
+++ events.js
@@ -1,18 +1,24 @@
 var events = require('events');
 
 
-var First = function(number){
-	var event = new events.EventEmitter();
-	if (number){
-		event.emit('end', number);
-	}
-};
+// var First = function(number){
+// 	var event = new events.EventEmitter();
+// 	if (number){
+// 		event.emit('end', number);
+// 	}
+// };
 
-var first = First(8);
-first.on('end', function(a){
-	console.log(a);
-});
+// var first = First(8);
+// first.on('end', function(a){
+// 	console.log(a);
+// });
 
 // var x = event.emit('end', 3);
 
 
+var array = ['one', 'two', 'three'];
+function logEm(element, index, array){
+  console.log(index + ':' + element);
+};
+
+array.forEach(logEm);
